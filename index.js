90011a569f072b3a31341fc6b972b00e7792eafb
Mridul <mridulpal19@gmail.com>
2022-05-17 19:25:17 +0530

    set up view engine

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const port=8000; //80 for production
 
 // use express router 
 
+
+
 app.use('/',require('./routes'))
+
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if (err){
         console.log(`Error in running the server:${err}`);
