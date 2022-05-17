8f6f8ba67094578312b0930d4007f1b067b6c4c5
Mridul <mridulpal19@gmail.com>
2022-05-17 18:09:44 +0530

    first commit :create directory

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+const port=8000; //80 for production
+
+app.listen(port,function(err){
+    if (err){
+        console.log(`Error in running the server:${err}`);
+    }
+    console.log(`Server is running on port No. ${port}`)
+})
+
