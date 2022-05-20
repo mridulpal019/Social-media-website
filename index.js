dea29690e601ae1dddbfaa14f9980b5887918331
Mridul <mridulpal19@gmail.com>
2022-05-20 13:43:27 +0530

    install ejs layout and set up assests and layout

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express=require('express');
 const app=express();
 const port=8000; //80 for production
+const expressLayouts= require('express-ejs-layouts');
+//static files
 
+app.use(express.static("./assets"))
 
+//before route
+app.use(expressLayouts);
+//extract style adn scripts into the layott
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 // use express router 
 
 
