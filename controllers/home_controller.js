a364135c701b54a975f0f0f48281cec43cee86d3
Mridul <mridulpal19@gmail.com>
2022-05-25 04:46:42 +0530

    show comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,7 +10,15 @@ module.exports.home =function(req,res){
     // });
 
 //populate the user for each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
             title:"Codeial |home",
             posts:posts
