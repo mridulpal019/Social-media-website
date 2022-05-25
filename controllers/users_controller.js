855d24a0be64b8c56ad20b2ff9d366b673f171e6
Mridul <mridulpal19@gmail.com>
2022-05-26 02:10:00 +0530

    update profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -15,6 +15,16 @@ module.exports.profile= function(req,res){
        
 }
 
+module.exports.update =function(req,res){
+    if (req.user.id ==req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+            return res.redirect('back');
+        })
+    }else{
+        return res.status(401).send('Unauthorised');
+    }
+}
+
 module.exports.signUp = function(req,res){
     if (req.isAuthenticated()){
         return res.redirect('/users/profile')
